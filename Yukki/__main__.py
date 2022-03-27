b8780785ec99dfdb2a542b1e8797c25de6095c0c
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-03-27 12:58:53 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -236,13 +236,13 @@ A Telegram Music+Video Streaming bot with some useful features.
 All commands can be used with: / """
 
 
-@app.on_message(filters.command("vchelp") & filters.private)
+@app.on_message(filters.command("help") & filters.private)
 async def vchelp_command(_, message):
     text, keyboard = await lovely_parser(message.from_user.mention)
     await app.send_message(message.chat.id, text, reply_markup=keyboard)
 
 
-@app.on_message(filters.command("vcstart") & filters.private)
+@app.on_message(filters.command("start") & filters.private)
 async def vcstart_command(_, message):
     if len(message.text.split()) > 1:
         name = (message.text.split(None, 1)[1]).lower()
